2068df6ca508fb2ed4064bfdd808366ba55db5ff
Miras-zozh <mmiras0207@gmail.com>
2025-09-02 00:52:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,8 @@ function updateLanguageUI() {
       const dbTitleEl = document.getElementById('db-title');
   if (dbTitleEl && translations[currentLanguage] && translations[currentLanguage].dbTitle) {
     dbTitleEl.textContent = translations[currentLanguage].dbTitle;
-  });
+  }
+    
     document.querySelectorAll('[data-i18n]').forEach(el => {
       const key = el.getAttribute('data-i18n');
       if (translations[currentLanguage][key]) {
